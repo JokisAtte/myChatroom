e74af012cf759f097ac15b94a69734eabc0d30e8
Atte Jokinen <atte.jokinen95@gmail.com>
2025-07-02 14:10:08 +0300

    Try building responsive ui

---
+++ lib/core/constants/breakpoints.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppBreakpoints {
+  static const double mobile = 100.0;
+
+  AppBreakpoints._();
+}
+
+extension ResponsiveBreakpoints on BuildContext {
+  /// Returns the current screen width.
+  double get screenWidth => MediaQuery.of(this).size.width;
+
+  /// Returns the current screen height.
+  double get screenHeight => MediaQuery.of(this).size.height;
+
+  /// Checks if the current screen width is considered a mobile size.
+  bool get isMobile => screenWidth < AppBreakpoints.mobile;
+}
